157f24f5802d66866379b8c9ec9b27032188a0d1
61milenap <61milenap8678@gmail.com>
2022-04-12 20:36:30 +0500

    321

--- keyboard.py
+++ keyboard.py
@@ -1,2 +1,9 @@
 from aiogram import Bot, Dispatcher, executor, types
 import random
+
+def keyboard_no_yes():
+    keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='Да')
+    key_no = types.InlineKeyboardButton(text='Нет', callback_data='Нет')
+    keyboard.add(key_yes, key_no)
+    return keyboard
